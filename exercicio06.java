3058a45a65b44431b3ebf9d079224963b22fb0fd
lucasBelisario82 <lbelisario82@gmail.com>
2026-03-10 10:10:46 -0300

    Add files via upload

---
+++ exercicio06.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;   //Lucas Belisario - 1261929907
+
+public class exercicio06.java
+
+public static void main (String[] args){
+    double numA;
+    double numB;
+    double resultado;
+    
+    Scanner entrada = new Scanner (System.in);
+
+    System.out.printf("Digite a base do retângulo:    ");
+    numA = entrada.nextDouble();
+    Syste.out.printf("digite a altura do retângulo:   ");
+    numB = entrada.nextdouble();
+    resultado = numA * numB;
+
+    system.out.printf("A area do seu retângulo é:   "+ resultado);
+
+
+    
+}
